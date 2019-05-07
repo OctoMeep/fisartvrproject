dea89965c83c48662941ec610e6ddf318aa13615
OctoMeep <roboneleven@gmail.com>
2019-05-07 16:46:53 +0200

    import map teleportation scripts, from test project

---
+++ Assets/Scripts/TeleportHome.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+
+public class TeleportHome : MonoBehaviour {
+
+	public SteamVR_Action_Boolean homeAction;
+
+	public GameObject home;
+
+	private float downSince = 0f;
+
+	public float delay = 1f;
+
+	private bool down = false;
+
+	public void Update() {
+		if (homeAction.GetState(SteamVR_Input_Sources.Any)) {
+			if(down){
+				if(Time.time - downSince >= delay){
+					print("yeet");
+					teleport();
+					down = false;
+				}
+			}
+			else{
+				down = true;
+				downSince = Time.time;
+			}
+		}
+		else {
+			down = false;
+		}
+	}
+	
+	private void teleport(){
+		transform.position = home.transform.position;
+	}
+}
---
+++ Assets/Scripts/mButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mButton : MonoBehaviour {
+
+	public GameObject r;
+	private GameObject p;
+
+	public void Start(){
+		p = GameObject.FindWithTag("Player");
+	}
+
+	public void pressed(){
+		p.transform.position = r.transform.position;
+	}
+}
