bd697fb24de233cbf2cf020ac93bfae1570fb8fd
OctoMeep <roboneleven@gmail.com>
2019-06-04 16:31:58 +0200

    nothing

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+
+    public GameObject Destination;
+
+    public GameObject Object;
+
+    public void go(){
+        Object.transform.position = Destination.transform.position;
+    }
+}
